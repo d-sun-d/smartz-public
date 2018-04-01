ed6c9eacd6d23a2225a8750b38d06dacb873eacc
Alexander Ryabchenko <d-sun-d@yandex-team.ru>
2018-04-01 11:17:40 +0300

    add one more sample

---
+++ constructor_examples/dictator.py
@@ -0,0 +1,67 @@
+from smartz.api.constructor_engine import ConstructorInstance
+from smartz.eth.contracts import make_generic_function_spec, merge_function_titles2specs
+
+
+class Constructor(ConstructorInstance):
+    def get_params(self):
+        json_schema = {
+            "type": "object",
+            "additionalProperties": False,
+        }
+
+        ui_schema = {}
+
+        return {
+            "result": "success",
+            "schema": json_schema,
+            "ui_schema": ui_schema
+        }
+
+
+    def construct(self, fields):
+        source = self.__class__._TEMPLATE
+        return {
+            "result": "success",
+            'source': source,
+            'contract_name': "DictatorVouting"
+        }
+
+
+    def post_construct(self, fields, abi_array):
+        function_titles = {
+            'voteResult': {
+                'title': 'Vote result',
+                'description': 'Vote of dictator'
+            },
+
+            'dictatorVoting': {
+                'title': 'Vote',
+                'description': 'You are dictator make your vote (true of false)',
+                'inputs': [{
+                    'title': 'vote',
+                }]
+            }
+        }
+        return {
+            "result": "success",
+            'function_specs': merge_function_titles2specs(make_generic_function_spec(abi_array), function_titles),
+            'dashboard_functions': ['voteResult', 'dictatorVoting']
+        }
+
+
+    # language=Solidity
+    _TEMPLATE = """
+pragma solidity ^0.4.0;
+contract SmartVote {
+    address dictator;
+    bool dictatorVote;
+
+    function dictatorVoting(bool vote) public {
+        dictatorVote = vote;
+    }
+
+    function voteResult() public constant returns (bool _voteResult) {
+        _voteResult = dictatorVote;
+    }
+
+}"""
